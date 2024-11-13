e81a3e0773d93e24ee5f12b10848e7115ef40801
PANKAjBamniya <bamniya853@gmail.com>
2024-11-13 09:08:24 +0530

    nameupdate

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const navbar = () => {
     <div className="n-wrapper" id="Navbar">
       {/* left */}
       <div className="n-left">
-        <div className="n-name">Pnkj</div>
+        <div className="n-name">Pankaj</div>
         <Toggle />
       </div>
       {/* right */}
