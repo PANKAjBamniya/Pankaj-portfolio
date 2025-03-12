6b676d68607e6de7e0fb4ccb8f8bc960c0ad17e0
PaNkaj <147901370+PANKAjBamniya@users.noreply.github.com>
2025-03-12 17:52:23 +0530

    testimonial

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -5,8 +5,8 @@ import "swiper/css";
 
 import { Pagination } from "swiper";
 import "swiper/css/pagination";
-import profilePic1 from "../../img/profile2.jpg";
-import profilePic2 from "../../img/profile2.jpg";
+import profilePic1 from "../../img/kisan.jpeg";
+import profilePic2 from "../../img/sarfraj.jpeg";
 import profilePic3 from "../../img/profile3.jpg";
 import profilePic4 from "../../img/profile4.jpg";
 
@@ -15,22 +15,22 @@ const Testimonial = () => {
     {
       img: profilePic1,
       review:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+        "Amazing service! The team was highly professional and delivered beyond my expectations. I highly recommend them for web development projects.",
     },
     {
       img: profilePic2,
       review:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+        "Amazing service! The team was highly professional and delivered beyond my expectations. I highly recommend them for web development projects.",
     },
     {
       img: profilePic3,
       review:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+        "Amazing service! The team was highly professional and delivered beyond my expectations. I highly recommend them for web development projects.",
     },
     {
       img: profilePic4,
       review:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+        "Amazing service! The team was highly professional and delivered beyond my expectations. I highly recommend them for web development projects.",
     },
   ];
 
