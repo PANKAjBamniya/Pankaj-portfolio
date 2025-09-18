11456903cd0278781063528b1b2e42e6ac3db745
PaNkaj <147901370+PANKAjBamniya@users.noreply.github.com>
2025-09-18 18:11:16 +0530

    porfolio-project
    update all projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,59 +5,74 @@ import "swiper/css";
 import Sidebar from "../../img/em-project.png";
 import Ecommerce from "../../img/job project .png";
 import labcare from "../../img/labcare.png";
+import real from "../../img/real1.png";
 import MusicApp from "../../img/khatabook.png";
 import empl from "../../img/empl.png";
 import dash from "../../img/dash.png";
 import { themeContext } from "../../Context";
 import { Autoplay } from "swiper";
-
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   return (
     <div className="portfolio" id="projects">
+      
       {/* heading */}
       <span style={{ color: darkMode ? "white" : "" }}>Recent Projects</span>
-
       {/* slider */}
       <Swiper
         spaceBetween={30}
         slidesPerView={3}
         grabCursor={true}
         loop={true}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
         modules={[Autoplay]}
         className="portfolio-slider"
       >
+        
         <SwiperSlide>
+          
           <a href="https://eminenceacademy.netlify.app" target="_blank">
+            
             <img src={Sidebar} alt="" />
           </a>
         </SwiperSlide>
         <SwiperSlide>
+          
           <a href="https://emsystemdev.netlify.app/" target="_blank">
+            
             <img src={empl} alt="" />
           </a>
         </SwiperSlide>
         <SwiperSlide>
+          
           <a href="https://1700ebooks.netlify.app/" target="_blank">
+            
             <img src={labcare} alt="" />
           </a>
         </SwiperSlide>
         <SwiperSlide>
+          
+          <a href="https://nextgendev.netlify.app/" target="_blank">
+            
+            <img src={real} alt="" />
+          </a>
+        </SwiperSlide>
+        <SwiperSlide>
+          
           <img src={Ecommerce} alt="" />
         </SwiperSlide>
         <SwiperSlide>
+          
           <a href="https://khatabookdev.netlify.app" target="_blank">
+            
             <img src={MusicApp} alt="" />
           </a>
         </SwiperSlide>
-
         <SwiperSlide>
-          <a href="desig-dashboard.netlify.app" target="_blank">
+          
+          <a href="https://desig-dashboard.netlify.app/" target="_blank">
+            
             <img src={dash} alt="" />
           </a>
         </SwiperSlide>
